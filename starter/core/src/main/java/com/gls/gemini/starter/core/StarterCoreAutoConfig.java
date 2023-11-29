4be3fda038bf6055c2857e7d5097e08c6fcb99d7
李智勇 <george510257@gmail.com>
2023-11-29 17:26:01 +0800

    init

---
+++ starter/core/src/main/java/com/gls/gemini/starter/core/StarterCoreAutoConfig.java
@@ -0,0 +1,9 @@
+package com.gls.gemini.starter.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class StarterCoreAutoConfig {
+}
---
+++ starter/core/src/main/java/com/gls/gemini/starter/core/config/StarterCoreConfig.java
@@ -0,0 +1,9 @@
+package com.gls.gemini.starter.core.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StarterCoreConfig {
+
+
+}
