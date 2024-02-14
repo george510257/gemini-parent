d5f1013667576c4154c14d9b320d378d9a254009
李智勇 <george510257@gmail.com>
2024-02-14 16:05:26 +0800

    add :: excel starter

--- gemini-project/gemini-starter/gemini-starter-mybatis/src/main/java/com/gls/gemini/starter/mybatis/config/MybatisConfig.java
+++ gemini-project/gemini-starter/gemini-starter-mybatis/src/main/java/com/gls/gemini/starter/mybatis/config/MybatisConfig.java
@@ -1,5 +1,11 @@
 package com.gls.gemini.starter.mybatis.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -7,4 +13,24 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class MybatisConfig {
+    /**
+     * 分页插件
+     *
+     * @return 分页插件
+     */
+    @Bean
+    @ConditionalOnMissingBean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 添加乐观锁插件
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        // 添加数据权限插件
+//        interceptor.addInnerInterceptor(new DataPermissionInterceptor());
+        // 添加租户插件
+//        interceptor.addInnerInterceptor(new TenantLineInnerInterceptor());
+        // 添加分页插件
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
 }
