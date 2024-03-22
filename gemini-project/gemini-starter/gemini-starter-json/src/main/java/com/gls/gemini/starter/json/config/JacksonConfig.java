f2e01125c258aca4f3b4f1c77896cff082ae990c
李智勇 <george510257@gmail.com>
2024-03-22 10:17:11 +0800

    设置json数据格式

--- gemini-project/gemini-starter/gemini-starter-json/src/main/java/com/gls/gemini/starter/json/config/JacksonConfig.java
+++ gemini-project/gemini-starter/gemini-starter-json/src/main/java/com/gls/gemini/starter/json/config/JacksonConfig.java
@@ -1,12 +1,13 @@
 package com.gls.gemini.starter.json.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.gls.gemini.starter.json.support.DefaultDateFormat;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 
 import java.time.ZoneId;
@@ -26,7 +27,7 @@ public class JacksonConfig {
      * @return 自定义配置类
      */
     @Bean
-    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer(List<SimpleModule> simpleModules) {
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer(List<Module> modules) {
         return builder -> {
             // 设置全局地区
             builder.locale(Locale.CHINA);
@@ -35,7 +36,7 @@ public class JacksonConfig {
             // 设置全局日期格式
             builder.dateFormat(new DefaultDateFormat());
             // 设置全局序列化模块 JavaTimeModule 用于序列化和反序列化 java8 时间类型
-            builder.modules(simpleModules.toArray(new SimpleModule[0]));
+            builder.modules(modules);
             // 反序列化时忽略未知属性
             builder.failOnUnknownProperties(false);
             // 序列化时忽略值为 null 的属性
@@ -44,7 +45,7 @@ public class JacksonConfig {
     }
 
     @Bean
-    @Order(1)
+    @Order(Ordered.HIGHEST_PRECEDENCE)
     public JavaTimeModule javaTimeModule() {
         return new JavaTimeModule();
     }
