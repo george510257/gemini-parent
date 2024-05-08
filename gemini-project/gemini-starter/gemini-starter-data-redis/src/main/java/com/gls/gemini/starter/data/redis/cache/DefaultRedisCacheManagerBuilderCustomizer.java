e40e05b80bdb4597d3c3445ab995bf2bc1e226a3
李智勇 <lizy19@xiaopeng.com>
2024-05-08 11:06:56 +0800

    修改spring cache 配置

--- gemini-project/gemini-starter/gemini-starter-data-redis/src/main/java/com/gls/gemini/starter/data/redis/cache/DefaultRedisCacheManagerBuilderCustomizer.java
+++ gemini-project/gemini-starter/gemini-starter-data-redis/src/main/java/com/gls/gemini/starter/data/redis/cache/DefaultRedisCacheManagerBuilderCustomizer.java
@@ -2,6 +2,7 @@ package com.gls.gemini.starter.data.redis.cache;
 
 import com.gls.gemini.starter.data.redis.constants.RedisProperties;
 import jakarta.annotation.Resource;
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * 默认Redis缓存管理器构建器自定义器
@@ -20,6 +22,8 @@ import java.util.Map;
 public class DefaultRedisCacheManagerBuilderCustomizer implements RedisCacheManagerBuilderCustomizer {
     @Resource
     private RedisProperties redisProperties;
+    @Resource
+    private CacheProperties cacheProperties;
     @Resource
     private CacheExpireAnnotationProcessor cacheExpireAnnotationProcessor;
     @Resource
@@ -30,7 +34,9 @@ public class DefaultRedisCacheManagerBuilderCustomizer implements RedisCacheMana
         // 默认缓存配置
         RedisCacheConfiguration config = builder.cacheDefaults()
                 .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.string()))
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jsonRedisSerializer));
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jsonRedisSerializer))
+                // 设置缓存前缀 cacheName:
+                .computePrefixWith(cacheName -> Optional.ofNullable(cacheProperties.getRedis().getKeyPrefix()).orElse("") + cacheName + ":");
         builder.cacheDefaults(config);
 
         // 从配置文件中读取缓存配置
