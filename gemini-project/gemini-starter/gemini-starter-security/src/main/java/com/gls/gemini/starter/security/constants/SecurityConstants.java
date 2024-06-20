0a9af15acea46ddcff62f5943bc175dddc3f2379
李智勇 <lizy19@xiaopeng.com>
2024-06-20 17:11:54 +0800

    format pom

--- gemini-project/gemini-starter/gemini-starter-security/src/main/java/com/gls/gemini/starter/security/constants/SecurityConstants.java
+++ gemini-project/gemini-starter/gemini-starter-security/src/main/java/com/gls/gemini/starter/security/constants/SecurityConstants.java
@@ -1,5 +1,11 @@
 package com.gls.gemini.starter.security.constants;
 
+/**
+ * 安全常量
+ */
 public interface SecurityConstants {
+    /**
+     * 默认登录页面
+     */
     String JWK_SET_ENDPOINT = "/oauth2/jwks";
 }
